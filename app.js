096c35767a6d1b00b679ca07cb1853a410461b99
essa eayd abu kheir <esaaeyad22@gmail.com>
2022-04-20 06:59:59 -0700

    new-task

--- app.js
+++ app.js
@@ -14,10 +14,10 @@ var confirm  = prompt("you wants to skip the welcoming message ?");
 if ( confirm == 'no'){
     
         if ( gender =="male"){
-            alert(" Mr "+ "" + name   + "" +"welcome to my website ")
+            alert(" Mr "+ name  +" welcome to my website ")
 
         }else if ( gender =="female"){
-            alert(" Ms "+ "" + name + "" + "welcome to my website ")
+            alert(" Ms " + name +  " welcome to my website ")
         }else{
             alert( name +" Welcome to my website ")
         }
@@ -25,7 +25,35 @@ if ( confirm == 'no'){
 
     
    
-    
+   
+/*let l1 = [1,2,3,4,5,6] 
+function Maxer(lists){
+    let max = 0
+    for (let i=0; i < lists.length ; i++)
+    {
+        let item = lists[i]
+        if(max<item){
+            max=item
+        }
+    }
+    return max;
+}  
+console.log(Maxer(l1))*//*
+let l2 =[1,3,"tfdr","543",4]
+function sumNumber(lists){
+        let conter=0
+        for (let i=0; i < lists.length ; i++){
+            let item = lists[i]
+            
+            if("number"==typeof item){
+                conter=conter + item
+                
+            }
+        }
+        return conter
 
+
+}
+console.log(sumNumber(l2))*/
 
 
